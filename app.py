a95dfe1dfa8182fbc980c3348e2e0156b5516e59
jose690 <31294367+jose690@users.noreply.github.com>
2024-07-17 15:45:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 # Importa las clases y funciones necesarias de Flask, y otros módulos
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, render_template
 from config import Config
-from models import db, Usuario
+from models import db
 import pyodbc
-import os
 
 # Crea una instancia de Flask
 app = Flask(__name__)
@@ -15,21 +14,21 @@ db.init_app(app)
 # Define una ruta para la página principal
 @app.route('/')
 def index():
-    # Envía el archivo 'index.html' desde la raíz del proyecto
-    return send_from_directory(os.getcwd(), 'index.html')
+    # Renderiza la plantilla 'index.html'
+    return render_template('index.html')
 
 # Define una ruta para obtener datos de un usuario usando una consulta SQL directa
 @app.route('/obtener_datos_sql', methods=['GET'])
 def obtener_datos_sql():
     # Obtiene el valor de 'cedula' de los parámetros de la solicitud
-    cedula = request.args.get('cedula')
+    cedula = request.args.get('cedula').replace('-', '')  # Elimina los guiones
     # Cadena de conexión a la base de datos
     connection_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=201.237.248.149;DATABASE=PracticaUtilidades;UID=pruebas2016;PWD=pruebas2016'
     # Establece una conexión con la base de datos
     conn = pyodbc.connect(connection_string)
     # Crea un cursor para ejecutar la consulta
     cursor = conn.cursor()
-    # Ejecuta el procedimiento almacenado 'JoseGuzmanObtenerDatosPorCedula' con el parámetro 'cedula'
+    # Ejecuta el procedimiento almacenado 'ObtenerDatosPorCedula_JoseGuzman' con el parámetro 'cedula'
     cursor.execute('EXEC ObtenerDatosPorCedula_JoseGuzman ?', cedula)
     # Obtiene la primera fila del resultado de la consulta
     row = cursor.fetchone()
